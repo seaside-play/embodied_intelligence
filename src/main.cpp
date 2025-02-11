fa729698d8c4ca9282dc6f1fa31e162864b46cc0
chaojiang.wu <chaojiang.wu@peitian.com>
2025-02-11 16:23:22 +0800

    实现执行跨平台选择编译选项

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,29 @@
 #include <iostream>
 #include "sun/earth/basic.hpp"
 
+void TestCompileDefinition();
+
 int main(int agrc, char** argv) {
     std::cout << "hello world!\n";
 
     std::cout << "3 + 2 = " << Add(3, 2) << std::endl;
+
+    TestCompileDefinition();
+
     return 0;
+}
+
+void TestCompileDefinition() {
+#ifdef SUN_TARGET
+    std::cout << "SUN_TARGET is defined" << std::endl;
+#else
+    std::cout << "SUN_TARGET is not defined " << std::endl;
+#endif
+
+
+#if SUN_TARGET == gnulinux
+    std::cout << "now is gunlinux\n";
+#else
+    std::cout << "now is not gunlinux\n";
+#endif
 }
